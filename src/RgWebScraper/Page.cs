d58e5968e0ba4ae593c9043ed8ccb56c0fdf5e47
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-02-06 14:32:53 +0200

    Add models

---
+++ src/RgWebScraper/Page.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RgWebScraper
+{
+    public class Page
+    {
+        public string Url { get; set; }
+
+        public Page()
+        {
+
+        }
+    }
+}
---
+++ src/RgWebScraper/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RgWebScraper
+{
+    public class User
+    {
+        public string NameAndSurname { get; set; }
+
+        public float Score { get; set; }
+
+        public int HIndex { get; set; }
+
+        public int HIndexExcluding { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
