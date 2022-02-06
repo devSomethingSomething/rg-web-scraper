0b8b2031ea01e3961fba99969e09f3abe596598d
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-02-06 14:06:56 +0200

    Refactor code

--- src/RgWebScraper/Program.cs
+++ src/RgWebScraper/Program.cs
@@ -9,33 +9,68 @@ namespace RgWebScraper
 {
     public class Program
     {
+        private static string email;
+        private static string password;
+
+        private static ChromeDriver chromeDriver;
+
+        private static async Task GetAccountDetails()
+        {
+            string[] lines = await File.ReadAllLinesAsync("Account.txt");
+
+            email = lines[0];
+            password = lines[1];
+        }
+
+        private static void SetupDriver()
+        {
+            chromeDriver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory)
+            {
+                Url = "https://www.researchgate.net/login?_sg=lISBBaecxrbK5CzzqaJWG-BMVZPU_5beI-xo58qDceWN3vH9As11qGbZxs4Esd52bidNgJqEobs0gWQNJiN4YQ"
+            };
+        }
+
+        private static void Login()
+        {
+            IWebElement element = chromeDriver.FindElement(By.Id("input-login"));
+            element.SendKeys(email);
+
+            element = chromeDriver.FindElement(By.Id("input-password"));
+            element.SendKeys(password);
+
+            element = chromeDriver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[1]/div/div/div/div/form/div/div[4]/button"));
+            element.Click();
+        }
+
         private static async Task Main()
         {
             // Guide
             // http://www.macaalay.com/2016/07/06/scraping-website-data-that-needs-you-to-log-in/
 
-            string[] lines = await File.ReadAllLinesAsync("Account.txt");
+            // Get the account details for login
+            await GetAccountDetails();
 
-            ChromeDriver chromeDriver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory)
-            {
-                Url = "https://www.researchgate.net/login?_sg=lISBBaecxrbK5CzzqaJWG-BMVZPU_5beI-xo58qDceWN3vH9As11qGbZxs4Esd52bidNgJqEobs0gWQNJiN4YQ"
-            };
+            // Setup the browser driver
+            SetupDriver();
 
+            // Leave this here for now
             chromeDriver.Navigate();
 
-            IWebElement element = chromeDriver.FindElement(By.Id("input-login"));
-            element.SendKeys(lines[0]);
+            // Login to allow certain pages to show, only needs to happen once as the browser will remember the login
+            // unless we somehow close the window
+            Login();
 
-            element = chromeDriver.FindElement(By.Id("input-password"));
-            element.SendKeys(lines[1]);
-
-            element = chromeDriver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[1]/div/div/div/div/form/div/div[4]/button"));
-            element.Click();
-
+            // Wait for a few seconds to prevent the bot check from finding the scraper
+            // 5 seconds
             Thread.Sleep(5000);
 
+            // --- Main loop should start here
+            // Read in URLs and go through them here
+
+            // Make sure to append the extra bit of the URL
             chromeDriver.Navigate().GoToUrl("https://www.researchgate.net/profile/James-Swan-3" + "/scores");
 
+            // Should store these values
             // RG Score
             Console.WriteLine(chromeDriver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[1]/div/div/div[3]/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div/div[2]")).Text);
 
@@ -45,10 +80,21 @@ namespace RgWebScraper
             // h-index excluding self-citations
             Console.WriteLine(chromeDriver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[1]/div/div/div[3]/div[1]/div/div/div[3]/div/div/div/div[2]/div[2]/div/div[2]")).Text);
 
+            // Do this at the end of every read, 10 to 15 seconds should be good
+            // Wait for a few seconds to prevent the bot check from finding the scraper
+            // 5 seconds
             Thread.Sleep(5000);
 
+            // --- Main loop ends here
+            // Should write the results out to a .csv file
+
+            // Close the driver when done
             chromeDriver.Quit();
 
+            // Display alert message
+            Console.WriteLine("Success");
+
+            // Wait for input then close the program
             Console.ReadKey(true);
         }
     }
